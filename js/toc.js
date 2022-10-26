41d66ecba87db16cfa2eecfdc258f1dacfbf464f
moqiang02 <moqiang02@163.com>
2022-10-26 17:50:03 +0800

    Site updated: 2022-10-26 17:49:42

---
+++ js/toc.js
@@ -0,0 +1,12 @@
+(function($) {
+	$(".toc").hide();
+
+	// rex
+/* 	$(".toc-title").click(function(){
+		$(".toc").toggle();
+	}); */
+	
+	$("#toc").hover(function(){
+		$(".toc").toggle();
+	});
+})(jQuery);
